f895c78ca8a6c763b74ff737cd0446e16e3a8814
MaCeleste <mceleste.orona@gmail.com>
2023-03-01 09:33:43 +0000

    Deck built

--- main.py
+++ main.py
@@ -1,23 +1,94 @@
-from tkinter import *
-from PIL import ImageTk,Image
+import pygame, sys, random, requests
+import os.path
 
-root = Tk()
-root.title('PokeWars')
-root.iconbitmap('icon.png')
-root.geometry('1200x900')
+# Initialise pygame
 
-# Create frames for cards
+pygame.init()
 
-pc_frame = LabelFrame(root, bg='white', text = 'PC cards')
-pc_frame.grid(column=0, row=0)
-player_frame =LabelFrame(root, bg='white', text = 'Player cards')
-player_frame.grid(column=0, row=1)
+# Set caption and icon
 
-# Put cards in frames
+pygame.display.set_caption('PokeWars')
+icon = pygame.image.load('icon.png')
+pygame.display.set_icon(icon)
 
-pc_label = Label(pc_frame, text='')
-pc_label.pack()
-player_label = Label(player_frame, text='')
-player_label.pack()
+# Create the screen
 
-root.mainloop()
+screen = pygame.display.set_mode((1200, 900))
+
+# Define FPS
+
+clock = pygame.time.Clock()
+fps = 60
+
+# Retrieve Pokemon info from PokeAPI
+
+pokemon = requests.get('https://pokeapi.co/api/v2/pokemon').json()
+#print(pokemon)
+
+# Main game class
+
+class MAIN:
+    def __init__(self):
+        print('Hello')
+
+class Deck:
+    def __init__(self):
+        # A list that will contain id, name, height, weight, image
+        self.cards = []
+        self.build_deck()
+
+    
+    def build_deck(self):
+        for _ in range(14):
+            pokemon = dict()
+            # Select a random Pokemon id
+            while True:
+                id = random.randint(1,898)
+                # Check if the id already exists in the deck. If it already exists, select a new id
+                if not any(d['id'] == 'id' for d in self.cards):
+                    response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{id}').json()
+                    image_path = 'images/' + response["name"] + '.png'
+                    if os.path.isfile(image_path):
+                        break
+                    else:
+                        print(image_path)
+                        continue
+                else:
+                    continue
+            
+            #Retrive data from PokeAPI for the Pokemon corresponding to the selected id
+            #response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{pokemon["id"]}').json()
+
+            pokemon['id'] = id
+            pokemon['name'] = response['name']
+            pokemon['height'] = response['height']
+            pokemon['weight'] = response['weight']
+            pokemon['image'] = f'{pokemon["name"]}.png'
+
+            self.cards.append(pokemon)
+
+        print(self.cards)
+
+
+# Create instance of a new game
+
+main_game = MAIN()
+deck = Deck()
+
+# Game loop
+running = True
+while running:
+    # Event listener
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # Screen and fps
+    screen.fill((0, 0, 0))
+    clock.tick(fps)
+
+   
+
+    pygame.display.update()
